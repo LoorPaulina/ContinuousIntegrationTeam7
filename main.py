a990397e86eb19297d6317f5b3212f8217fee656
Andres <adborbor@espol.edu.ec>
2024-06-26 14:27:35 -0500

    membership availabilty requirement satisfied

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#code
+class GymMembership:
+    def isAvailable(self, membership):
+        if membership == None or membership not in self.membership_plans: 
+            raise ValueError("Membership not available")
+        return True
+    
+        
