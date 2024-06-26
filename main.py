8bf108d21b12f560a3bc6c62fd4e3a0d11b2527d
joemcara <94081168+joemcara@users.noreply.github.com>
2024-06-26 14:34:19 -0500

    joseph changed

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#code
+class GymMembership:
+
+    #yoser
+    def get_discount(self):
+        if self.total_cost > 400:
+            self.total_cost -= 50
+        elif self.total_cost > 200: 
+            self.total_cost -= 20 
+
+    def premiun_membership(self):
+        self.total_cost = self.total_cost * 1.5 #premium membership cost 50% more
