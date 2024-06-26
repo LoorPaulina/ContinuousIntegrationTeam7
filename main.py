246c97ad49dc0da892714712d61fd04915ca50d2
Katherine Tumbaco <93357018+katumbac@users.noreply.github.com>
2024-06-26 14:32:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ class GymMembership:
   def add_additional_features(self, features):
     for feature in features:
       if feature not in self.additional_features:
-        self.selected_features.append.(feature)
+        self.selected_features.append(feature)
         
